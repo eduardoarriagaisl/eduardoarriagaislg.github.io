623e137f51def9d8f7f0ada309cfecf2a56b12ca
eduardoarriagaisl <167341517+eduardoarriagaisl@users.noreply.github.com>
2024-04-17 12:58:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
             servers: {},
             events: {},
             texts: {},
-            players: [ichi],
+            players: [],
             current_language: 'fr',
             current_server_header: 'EmpireEx_3',
             current_event_name: window.sessionStorage.getItem('event') ?? '',
